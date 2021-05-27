a9942682db3d3219e8ddf1e94fa51089f55352e1
Shivam108 <shivam.patkar95@gmail.com>
2021-05-27 17:08:32 +0530

    Update AttendanceProject1.py

--- AttendanceProject1.py
+++ AttendanceProject1.py
@@ -34,7 +34,7 @@ for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
-print(classNames)
+# print(classNames)
 
 
 def findEncodings(images):
